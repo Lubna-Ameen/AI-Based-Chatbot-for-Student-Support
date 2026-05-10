b6ec04f1f04d51367d9e1b70b7e634ac788a6183
Fatma <16S1989@utas.edu.om>
2026-05-10 20:27:48 +0400

    User Model

--- server/index.js
+++ server/index.js
@@ -60,3 +60,51 @@ const mongoose = require("mongoose"); //Import Mongoose
 const UserModel = mongoose.model("users", UserSchema);
 module.exports = UserModel;
 
+//Register API
+app.post("/register", async (req, res) => {
+    try {
+        const {
+            name,
+            email,
+            password,
+            role,
+            studentId
+        } = req.body;
+        // Check Existing User
+        const existingUser = await UserModel.findOne({
+            email: email
+        });
+        if (existingUser) { //Prevents duplicate accounts using same email
+            return res.send({
+                success: false,
+                message: "User Already Exists"
+            });
+
+        }
+        // Encrypt Password
+        const hashedPassword = await bcrypt.hash(password, 10);//Converts password into secure encrypted form
+        // Create User
+        const newUser = new UserModel({
+            name,
+            email,
+            password: hashedPassword,
+            role,
+            studentId
+ 
+        });
+        await newUser.save(); //Save user to database
+        res.send({
+            success: true,
+            message: "Registration Successful"
+        });
+    }
+    catch (error) {
+        res.send({
+            success: false,
+            message: "Registration Error",
+            error: error.message
+        });
+    }
+});
+ 
+
