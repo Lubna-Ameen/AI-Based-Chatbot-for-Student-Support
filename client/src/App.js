30df40d0d7095617c5d5291638464c982b3eb214
Lubna Ameen <16S19162@utas.edu.om>
2026-05-11 18:24:52 +0400

    Frontend Pages Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import StudentLogin from "./pages/StudentLogin";
+import AdminLogin from "./pages/AdminLogin";
+import StudentRegister from "./pages/StudentRegister";
+import AdminRegister from "./pages/AdminRegister";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Home Page */}
+      <Home />
+
+      {/* Student Login */}
+      <StudentLogin />
+
+      {/* Admin Login */}
+      <AdminLogin />
+
+      {/* Student Register */}
+      <StudentRegister />
+
+      {/* Admin Register */}
+      <AdminRegister />
     </div>
   );
 }
